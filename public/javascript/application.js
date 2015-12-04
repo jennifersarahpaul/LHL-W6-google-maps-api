d650bb927cccc6ed040be3d57c3ad79fb847972b
Jennifer Tigner <jennifertigner@email.com>
2015-12-04 23:57:03 +0000

    Added two markers and gave them bouncy animations

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,36 @@
-$(document).ready(function() {
+function initMap() {
+  var home = {lat: 48.469, lng: -123.367}; 
+  var aussie = {lat: -25.363, lng: 131.044};
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+  var map = new google.maps.Map(document.getElementById('map'),  {
+    zoom: 12, 
+    center: home
+  }); 
+
+  var homeMarker = new google.maps.Marker({
+    position: home, 
+    map: map, 
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: 'Welcome home, Jenn & Garrett'
+  });
+
+  var aussieMarker = new google.maps.Marker({
+    position: aussie, 
+    map: map, 
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: 'Whee I am in Australia!'
+  });
+
+  homeMarker.addListener('click', toggleBounce);
+  aussieMarker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    merker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
