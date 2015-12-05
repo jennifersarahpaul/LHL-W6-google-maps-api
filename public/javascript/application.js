043a7a4740fcf5d9befcb282e0b9d58a79914c1b
Jennifer Tigner <jennifertigner@email.com>
2015-12-05 00:33:40 +0000

    Added markers for a few places around Victoria

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,6 +1,10 @@
 function initMap() {
   var home = {lat: 48.469, lng: -123.367}; 
   var aussie = {lat: -25.363, lng: 131.044};
+  var moonUnderWater = {lat: 48.438, lng: -123.376};
+  var kickboxing = {lat: 48.493, lng: -123.388};
+  var beach = {lat: 48.521, lng: -123.366};
+  var swanLake = {lat: 48.463, lng: -123.373};
 
   var map = new google.maps.Map(document.getElementById('map'),  {
     zoom: 12, 
@@ -21,6 +25,38 @@ function initMap() {
     draggable: true,
     animation: google.maps.Animation.DROP,
     title: 'Whee I am in Australia!'
+  });
+
+  var moonUnderWaterMarker = new google.maps.Marker({
+    position: moonUnderWater, 
+    map: map, 
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: 'Moon Under Water Pub'
+  });
+
+  var kickboxingMarker = new google.maps.Marker({
+    position: kickboxing, 
+    map: map, 
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: '30 Minute Hit'
+  });
+
+  var beachMarker = new google.maps.Marker({
+    position: beach, 
+    map: map, 
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: 'My favourite beach... Cordova'
+  });
+
+  var swanLakeMarker = new google.maps.Marker({
+    position: swanLake, 
+    map: map, 
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    title: 'Swan Lake near my home'
   });
 
   var contentString = '<div id="content">'+
