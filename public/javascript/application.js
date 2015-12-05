7109efb38aa831c2a4ac9e8646a6041362e8e572
Jennifer Tigner <jennifertigner@email.com>
2015-12-05 00:21:54 +0000

    Created an infobox for the Victoria marker

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -23,8 +23,29 @@ function initMap() {
     title: 'Whee I am in Australia!'
   });
 
-  homeMarker.addListener('click', toggleBounce);
-  aussieMarker.addListener('click', toggleBounce);
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h2 id="firstHeading" class="firstHeading">Victoria</h2>'+
+      '<div id="bodyContent">'+
+      '<p><b>Victoria</b> /vɪkˈtɔriə/ is the capital city of ' +
+      'British Columbia, Canada, and is located on the southern tip ' +
+      'of Vancouver Island off Canada\'s Pacific coast. The city has a ' +
+      'population of about 80,017, while the metropolitan area of ' +
+      'Greater Victoria, has a population of 344,615, making it the ' +
+      '15th most populous Canadian urban region. </p>' +
+      '<p>Read more: <a href="https://en.wikipedia.org/wiki/Victoria,_British_Columbia">'+
+      'https://en.wikipedia.org/wiki/Victoria,_British_Columbia</a> '+
+      '(last visited Dec 4, 2015).</p>'+
+      '</div>';
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  homeMarker.addListener('click', function() {
+    infoWindow.open(map, homeMarker);
+  });
 }
 
 function toggleBounce() {
